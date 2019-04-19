8b7e3646d060f4d6807531364406245d7f4ce56e
BaiJiFeiLong <baijifeilong@gmail.com>
2019-04-19 13:03:43 +0800

    Init project

---
+++ src/main/java/io/github/baijifeilong/foundation/example/BaijifeilongFoundationExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.baijifeilong.foundation.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaijifeilongFoundationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaijifeilongFoundationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/baijifeilong/foundation/example/BaijifeilongFoundationExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.baijifeilong.foundation.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaijifeilongFoundationExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
